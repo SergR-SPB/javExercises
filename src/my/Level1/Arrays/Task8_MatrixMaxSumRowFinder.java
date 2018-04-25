fc509c6895d506350c08e240a536a85c95f84701
SergR-SPB <37777794+SergR-SPB@users.noreply.github.com>
2018-04-26 01:07:31 +0300

    Прошу проверить : Написать метод поиска индекса строки с максимальной суммой элементов

--- src/my/Level1/Arrays/Task8_MatrixMaxSumRowFinder.java
+++ src/my/Level1/Arrays/Task8_MatrixMaxSumRowFinder.java
@@ -1,10 +1,10 @@
 package my.Level1.Arrays;
 /*
    Task 8 (MatrixMaxSumRowFinder)
-   Написать метод поиска индекса строки с максимальной суммой элементов.
-   Если таких строк несколько, вернуть индекс первой.
-   Если матрица пустая (нет ни одной строки),
-   бросить исключение IllegalArgumentException.
+   Прошу проверить : Написать метод поиска индекса строки с максимальной суммой элементов.
+              несделано: Если таких строк несколько, вернуть индекс первой.
+              несделано:    Если матрица пустая (нет ни одной строки),
+              несделано:    бросить исключение IllegalArgumentException.
    Предполагается, что матрица прямоугольная.
    int maxSumRowIndex(int[][] matrix)
    Пример работы:
@@ -15,45 +15,55 @@ package my.Level1.Arrays;
    }) == 1
 */
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Task8_MatrixMaxSumRowFinder {
 
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
 
+
     //Ввод размеров массива array [][]:
     public static int[] arraySize() {
         int[] arraySise = new int[2];
-        System.out.println("Input array size one");
+        System.out.println("Input array size one:");
         int sizeOne = scanner.nextInt();
         arraySise[0] = sizeOne;
-        System.out.println("Input array size one");
+        System.out.println("Input array size one:");
         int sizeTwo = scanner.nextInt();
         arraySise[1] = sizeTwo;
         return arraySise;
     }
+
+    //Выбор метода заполнения массива [][] array
+    public static int[][] arrayChoice(int[] arraySize) {
+        System.out.println("Choice of array filling method:");
+        System.out.println("If \"1\" - console filling.");
+        System.out.println("If \"2\" - random filling.");
+        int n = scanner.nextInt();
+        if (n == 1) {
+            int[][] array = arrayFillingOfConsole(arraySize);
+            matrixArray(array);
+            return array;
+        } else {
+            int[][] array = arrayFilingOfrandom(arraySize);
+            matrixArray(array);
+            return array;
+        }
+    }
+
     //1. Заполнение массива array [][] из консоли.
-    public static int [][] arrayFillingOfConsole (int [] arraySize ){
-        int[][] ar1ray = new int[arraySize[0]][arraySize[1]];
-        for (int i=0; i< ar1ray.length; i++ ){
-            for (int j = 0; j< ar1ray[i].length; j++) {
+    public static int[][] arrayFillingOfConsole(int[] arraySize) {
+        int[][] array = new int[arraySize[0]][arraySize[1]];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.println("Input one array [" + i + "]; Input two array [" + j + "]");
-                ar1ray[i][j] = scanner.nextInt();
+                array[i][j] = scanner.nextInt();
             }
         }
-        /*int[][] matrix = new int[arraySize[0]][arraySize[1]];
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print("Input matrix row " + i + " element no: " + j + " : ");
-                matrix[i][j] = scanner.nextInt();
-            }
-        }*/
-        //System.out.println(Arrays.deepToString(array));
-        return ar1ray;
+        System.out.println(Arrays.deepToString(array));
+        return array;
     }
 
     //2. Заполнение массива array[][] случайными числами.
@@ -63,25 +73,59 @@ public class Task8_MatrixMaxSumRowFinder {
             for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = random.nextInt(10);
             }
-        }return  array;
+        }
+        return array;
     }
 
-    // Вывод на печать массива arrayFilingOf [][]
-    public static void matrixArray (int [][] array ){
-        for (int i=0; i<array.length; i++){
+    // Вывод на печать массива arrayFilingOf... [][] и вывод ID с максимальной суммой
+    public static void matrixArray(int[][] array) {
+        int max = 1;
+        int idMax = 1;
+
+        for (int i = 0; i < array.length; i++) {
             System.out.println();
-            for (int j=0; j<array[i].length;j++){
-                System.out.print(array [i][j]+" ");
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
             }
         }
+        System.out.println();
+
+    }
+
+    /* Создание int [] arraySumstring, где каждый элемент массива
+     сумма строки массива int [][] array.*/
+    public static int[] arraySumstring(int[][] array) {
+        int[] arraySumstring = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                arraySumstring[i] = arraySumstring[i] + array[i][j];
+            }
+        }
+        return arraySumstring;
+    }
+
+    // Метод поиска индекса строки с максимальной суммой элементов.
+    public static int idMaxarraySumstring(int[] arraySumstring) {
+        int max = arraySumstring[1];
+        int idMax = 1;
+        for (int i = 0; i < arraySumstring.length; i++) {
+            if (max < arraySumstring[i]) {
+                max = arraySumstring[i];
+                idMax = i;
+            }
+        }
+        return idMax;
     }
 
     public static void main(String[] args) {
-        int [] arraySize = arraySize();
+        int[] arraySize = arraySize();
         System.out.println(Arrays.toString(arraySize));
-        int [][] arrayFilingOfrandom = arrayFilingOfrandom (arraySize);
-        matrixArray (arrayFilingOfrandom);
-        int [][] arrayFillingOfConsole = arrayFillingOfConsole( arraySize );
-        //System.out.println(Arrays.deepToString(arrayFillingOfConsole));
+        int[][] array = arrayChoice(arraySize);
+        int[] arraySumstring = arraySumstring(array);
+        System.out.println(Arrays.toString(arraySumstring));
+        int idMax = idMaxarraySumstring(arraySumstring);
+        System.out.println(idMax);
+        //Дальше потерялся  - -с двумя одинаковыми значениями не получается
     }
 }
